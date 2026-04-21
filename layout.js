bd9d75011e40eedaf6827ae015da617c6f59add3
CoutinhoSUD <144079383+Coutinho33@users.noreply.github.com>
2026-04-21 20:54:14 -0300

    Delete layout.js

--- layout.js
+++
@@ -1,6 +0,0 @@
-
-export const metadata = { title: 'Núcleo de Estudos da Fé' };
-import './globals.css';
-export default function RootLayout({children}) {
- return <html lang="pt-br"><body>{children}</body></html>
-}
